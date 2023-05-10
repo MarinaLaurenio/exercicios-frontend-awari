71e90dd33d63bb06ece17c35b923fedb5af858bc
Marina Laurenio <laureniomarina@gmail.com>
2023-05-10 14:05:32 +0100

    primeiro commit - sem alterações

---
+++ praticaForm/script.js
@@ -0,0 +1,13 @@
+var form = document.getElementById("contact-form");
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var name = form.elements["Nome"].value;
+  var email = form.elements["E-mail"].value;
+  var mensagem = form.elements["Mensagem"].value;
+
+  var resultado = "Nome:" + name + "\nE-mail" + email + "\nMensagem: " + mensagem;
+  alert(resultado);
+}
+
+form.addEventListener("submit", handleSubmit);
